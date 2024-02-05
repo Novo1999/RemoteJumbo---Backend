c48f0e7224382d3abb0a8ebdfc8807837831a448
Novo1999 <neptune.navo@gmail.com>
2024-02-06 01:10:28 +0600

    update: star method

--- src/controller/user/star.ts
+++ src/controller/user/star.ts
@@ -1,10 +1,21 @@
-// import { Request, Response } from 'express'
-// import { StatusCodes } from 'http-status-codes'
-// import { BadRequestError, NotFoundError } from '../../errors/customError'
-// import Job from '../../model/job'
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import Job from '../../model/job'
 
-// export const starJob = async (req: Request, res: Response) => {
-//   const job = await Job.findOne({ _id: req.params.id })
+export const starJob = async (req: Request, res: Response) => {
+  const idToFind = req.params.id
+  const { userId: idToInsert } = req.body
+  console.log('ID: ' + idToFind)
+  const job = await Job.findOne({ _id: idToFind })
+  const starredIds = job.isStarred.userId
+  // pushing the user id to the jobs to set how many users have starred the job
+  if (starredIds.includes(idToInsert)) {
+    const indexToRemove = starredIds.findIndex((id) => id === idToInsert)
+    starredIds.splice(indexToRemove, 1)
+  } else {
+    starredIds.push(idToInsert)
+  }
+  await job.save()
 
-//   res.status(StatusCodes.OK).json(job)
-// }
+  res.status(StatusCodes.OK).json(job)
+}
--- src/routes/user/userJob.ts
+++ src/routes/user/userJob.ts
@@ -5,6 +5,7 @@ import { getJobs } from '../../controller/user/getJobs'
 import { getRandomJobs } from '../../controller/user/getRandomJobs'
 import { getSingleJob } from '../../controller/user/getSingleJob'
 import { searchJob } from '../../controller/user/searchJob'
+import { starJob } from '../../controller/user/star'
 
 const router = Router()
 
@@ -14,6 +15,7 @@ router
   .get('/total-jobs', getDocCounts)
   .get('/search', searchJob)
   .get('/random/:id', getRandomJobs)
+  .post('/star/:id', starJob)
   .get('/:id', getSingleJob)
   .patch('/:id', addViewCount)
 
